eb351b8aac031997995e0c8da1eed078804727f6
akko-0725 <157173275+akko-0725@users.noreply.github.com>
2025-02-16 13:47:24 +0900

    kadai020をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+// btnの取得
+const Btn = document.getElementById('btn')
+const Text = document.getElementById('text')
+
+Btn.addEventListener('click', () => {
+  Text.textContent = 'ボタンをクリックしました';
+});
